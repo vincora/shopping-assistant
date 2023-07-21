d6965341e66d5b6361f794f0785e7c8df525e47e
Yulia Glagola <julichis@mail.ru>
2023-07-21 22:17:30 +0400

    inputs validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import style from "./App.module.scss";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const onlyNumbers = ({ onChange, ...rest }) => {
+  const handleChange = (e) => {
+    let count = 0;
+    e.target.value = e.target.value
+      .replace(/[^\d.,]/g, "")
+      .replace(",", ".")
+      .replace(/[.]/g, () => (count++ == 0 ? "." : ""));
+
+    onChange.call(this, e);
+  };
+
+  return { onChange: handleChange, ...rest };
+};
+
+const schema = z.object({
+  amount: z.string().nonempty("This field is required"),
+  price: z.string().nonempty("This field is required"),
+});
 
 function App() {
   const [page, setPage] = useState(0);
@@ -9,7 +30,12 @@ function App() {
     price: 0,
     notes: "",
   });
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
   const onSubmit = (data) => {
     console.log(data);
     setItem(data);
@@ -19,8 +45,8 @@ function App() {
     <div className="App">
       <div className={style.container}>
         {page === 0 && (
-          <div className="">
-            {item.amount > 0 &&
+          <div>
+            {item.amount > 0 && (
               <div className={style.item}>
                 <div>Amount: {item.amount}</div>
                 <div>Price: {item.price}</div>
@@ -28,22 +54,38 @@ function App() {
                   Price per kg: {Number(item.price) / Number(item.amount)}
                 </div>
               </div>
-            }
+            )}
             <button onClick={() => setPage(1)}>add new item</button>
           </div>
         )}
         {page === 1 && (
-          <form className={style.inputs} onSubmit={handleSubmit(onSubmit)}>
-            <div className={style.title}>New item</div>
-            <input
-              type="text"
-              {...register("amount")}
-              placeholder="amount in kg"
-            />
-            <input type="text" {...register("price")} placeholder="price" />
-            <input type="text" {...register("notes")} placeholder="notes" />
-            <button type="submit">add</button>
-          </form>
+          <zodResolver>
+            <form className={style.inputList} onSubmit={handleSubmit(onSubmit)}>
+              <div className={style.title}>New item</div>
+              <label className={style.input}>
+                <p className={style.error}>{errors.amount?.message}</p>
+                <input
+                  {...onlyNumbers(register("amount"))}
+                  type="text"
+                  placeholder="amount in kg"
+                />
+              </label>
+              <label className={style.input}>
+                <p className={style.error}>{errors.price?.message}</p>
+                <input
+                  type="text"
+                  {...onlyNumbers(register("price"))}
+                  placeholder="price per kg"
+                />
+              </label>
+              <label className={style.input}>
+                <input type="text" {...register("notes")} placeholder="notes" />
+              </label>
+              <button type="submit" className={style.button}>
+                add
+              </button>
+            </form>
+          </zodResolver>
         )}
       </div>
     </div>
