e73bd7fa031a43ac061fa63de3adfbfeb8652bed
Yulia Glagola <julichis@mail.ru>
2023-08-15 15:00:05 +0400

    padding bottom on App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function App() {
     return (
-        <div className="max-w-sm mx-auto h-[100dvh] p-4 space-y-4 box-border">
+        <div className="max-w-sm mx-auto h-[100dvh] p-4 pb-12 box-border">
             <Outlet />
         </div>
     );
