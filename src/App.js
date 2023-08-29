3c172edbd85ba8d93013cd3311a12fa20fcbe9e0
Yulia Glagola <julichis@mail.ru>
2023-08-29 13:50:24 +0400

    more width on desktop

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function App() {
 
     return (
-        <div className="max-w-sm mx-auto h-[100dvh] p-6 box-border">
+        <div className="max-w-sm mx-auto h-[100dvh] p-6 box-border md:max-w-lg">
             <Outlet />
         </div>
     );
