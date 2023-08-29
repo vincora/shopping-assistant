f0c993abdb154563558b81e857108b289766c865
Yulia Glagola <julichis@mail.ru>
2023-08-29 19:50:50 +0400

    explanation text on first page

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -14,28 +14,35 @@ const FirstPage = () => {
             <h1 className="text-xl font-medium text-center mb-4">
                 Shopping assistant
             </h1>
-            <div className="grow overflow-y-scroll flex flex-col items-center gap-3 ">
-                {categories.map((category) => {
-                    const onCategoryDelete = () => {
-                        if (
-                            !window.confirm(
-                                `Do you want to delete ${category.category} category?`
-                            )
-                        ) {
-                            return false;
-                        }
-                        dispatch(deleteCategory(category.id));
-                    };
-                    return (
-                        <ActionDeleteElement
-                            key={category.id}
-                            onAction={onCategoryDelete}
-                        >
-                            <Category category={category} />
-                        </ActionDeleteElement>
-                    );
-                })}
-            </div>
+            {categories.length === 0 ? (
+                <div className="grow flex flex-col gap-3 justify-end items-center p-3 text-center">
+                    <div>Add a category of goods <br /> (tomatoes, rice, milk etc.)</div>
+                    <div className="icon-arrow-down"></div>
+                </div>
+            ) : (
+                <div className="grow overflow-y-scroll flex flex-col items-center gap-3 ">
+                    {categories.map((category) => {
+                        const onCategoryDelete = () => {
+                            if (
+                                !window.confirm(
+                                    `Do you want to delete ${category.category} category?`
+                                )
+                            ) {
+                                return false;
+                            }
+                            dispatch(deleteCategory(category.id));
+                        };
+                        return (
+                            <ActionDeleteElement
+                                key={category.id}
+                                onAction={onCategoryDelete}
+                            >
+                                <Category category={category} />
+                            </ActionDeleteElement>
+                        );
+                    })}
+                </div>
+            )}
             <div>
                 <CategoryForm />
             </div>
