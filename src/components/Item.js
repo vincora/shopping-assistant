953d9344c3ce4ae16fbbef20fce0eaa32526f4a6
Yulia Glagola <julichis@mail.ru>
2024-02-24 23:17:36 +0300

    added proper representation of links in notes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,31 @@
+import { Fragment } from "react";
+
 const Item = ({ item }) => {
+    const fullUrlRegex =
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b([-a-zA-Z0-9!@:%_+.~#?&/=]*)/gi;
+
+    const domainRegex =
+        /(www\.)?([-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b)/i;
+
+    const arrayFromNotesString = (str) => {
+        const matches = Array.from(str.matchAll(fullUrlRegex));
+
+        const indexes = matches.reduce((acc, match) => {
+            acc.push(match.index);
+            acc.push(match.index + match[0].length);
+
+            return acc;
+        }, []);
+
+        const checkpoints = [0, ...indexes, str.length];
+
+        let result = [];
+        for (let i = 0; i < checkpoints.length - 1; i++) {
+            result.push(str.substring(checkpoints[i], checkpoints[i + 1]));
+        }
+        return result;
+    };
+
     return (
         <div
             className="grid grid-cols-[2fr_1fr] p-3 md:p-5 text-sm bg-white border rounded"
@@ -11,7 +38,27 @@ const Item = ({ item }) => {
             <div>Price per kg/liter:</div>
             <div className="text-right">{item.pricePerUnit}</div>
             {item.notes && <div className="col-span-2">Notes:</div>}
-            <div className="col-span-2">{item.notes}</div>
+            {item.notes && (
+                <div className="break-words col-span-2">
+                    {arrayFromNotesString(item.notes).map((str, index) => {
+                        if (!fullUrlRegex.test(str)) {
+                            return <Fragment key={str + index}>{str}</Fragment>;
+                        }
+                        return (
+                            <a
+                                key={str + index}
+                                href={str}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="text-primary hover:underline underline-offset-2"
+                            >
+                                {" "}
+                                {str.match(domainRegex)[2]}
+                            </a>
+                        );
+                    })}
+                </div>
+            )}
         </div>
     );
 };
